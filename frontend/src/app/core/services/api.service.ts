0f24d9b9b34624f5f516f68469fab317969a2097
nicolasfuentesm19 <nicolasfuentesm19@gmail.com>
2026-06-15 22:14:40 -0400

    Actualizar IP publica del backend AWS

--- frontend/src/app/core/services/api.service.ts
+++ frontend/src/app/core/services/api.service.ts
@@ -5,5 +5,5 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
   // En producción esto debería venir de environments
-  public readonly baseUrl = 'http://localhost:8000';
+  public readonly baseUrl = 'http://98.89.29.74:8000';
 }
