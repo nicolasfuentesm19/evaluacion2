d8a02c64149aa3d34d4bee11596e1d4bcf5726b0
nicolasfuentesm19 <nicolasfuentesm19@gmail.com>
2026-06-16 01:01:17 -0400

    Update API IP to 3.93.197.168 and deploy frontend only

--- frontend/src/app/core/services/api.service.ts
+++ frontend/src/app/core/services/api.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  public baseUrl: string = 'http://44.203.246.12:8000'; // IP pública inyectada tras reiniciar Fargate
+  public baseUrl: string = 'http://3.93.197.168:8000'; // IP pública inyectada tras reiniciar Fargate
   constructor() { }
 }
